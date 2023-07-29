3e72a68a390d2f0fa6c12bba6e5f91c368592dd4
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-29 14:24:18 -0700

    DELETE /api/products/:id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,8 +16,6 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const data = await Product.findByPk(req.params.id, { include: [{ model: Category }, { model: Tag }] });
 
@@ -108,10 +106,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-
+    const success = await Product.destroy({ where: { id: req.params.id } });
+    if (success) {
+      res.status(200).json("Product deletet succesfully!");
+    } else {
+      res.status(404).json("No product with that ID exists!");
+    }
   } catch (err) {
     res.status(500).json(err.message);
   }
