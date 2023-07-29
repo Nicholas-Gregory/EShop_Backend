8ed6e2b7c44b2b7f6335611febeac4c396dc71e8
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-07-29 14:04:45 -0700

    GET /api/products/:id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const data = await Product.findAll({ include: [{ model: Category }, { model: Tag }] });
 
@@ -17,11 +15,17 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
+    const data = await Product.findByPk(req.params.id, { include: [{ model: Category }, { model: Tag }] });
 
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json("No product with that ID exists!");
+    }
   } catch (err) {
     res.status(500).json(err.message);
   }
